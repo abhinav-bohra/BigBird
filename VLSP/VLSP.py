bdb3877b0425332b9fdaee382cd589cedd6fa3e6
abhinavbohra@iitkgp.ac.in <abhinavbohra@iitkgp.ac.in>
2022-04-30 10:49:56 +0530

    Added VLSP dataset

--- VLSP/VLSP.py
+++ VLSP/VLSP.py
@@ -2,14 +2,18 @@
 
 import tensorflow_datasets as tfds
 
-# TODO(VLSP): Markdown description  that will appear on the catalog page.
+
 _DESCRIPTION = """
+Scientific papers datasets contains two sets of long and structured documents.
+
+  - input: the body of the document, pagragraphs seperated by "/n".
+  - output: the abstract of the document, pagragraphs seperated by "/n".
+  - meta_data: titles of sections, seperated by "/n".
+
 """
 
-# TODO(VLSP): BibTeX citation
-_CITATION = """
-"""
-
+_DOCUMENT = "input"
+_SUMMARY = "output"
 
 class Vlsp(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for VLSP dataset."""
@@ -26,14 +30,13 @@ class Vlsp(tfds.core.GeneratorBasedBuilder):
         builder=self,
         description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
-            # These are the features of your dataset like images, labels ...
+            _DOCUMENT: tfds.features.Text(),
+            _SUMMARY: tfds.features.Text(),
+            "meta_data": tfds.features.Text(),
         }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=None,  # e.g. ('image', 'label')
-        homepage='https://dataset-homepage/',
-        citation=_CITATION,
+        supervised_keys=(_DOCUMENT, _SUMMARY),
+        homepage=None,
+        citation=None,
     )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
@@ -41,15 +44,42 @@ class Vlsp(tfds.core.GeneratorBasedBuilder):
     # TODO(VLSP): Downloads the data and defines the splits
     # dl_manager is a tfds.download.DownloadManager that can be used to
     # download and extract URLs
+    # dl_paths = dl_manager.download_and_extract(_URLS)
+    path = os.path.join("VLSP")
     return [
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
-            # These kwargs will be passed to _generate_examples
-            gen_kwargs={},
+            gen_kwargs={"path": os.path.join(path, "train.json")},
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.VALIDATION,
+            gen_kwargs={"path": os.path.join(path, "val.json")},
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={"path": os.path.join(path, "test.json")},
         ),
     ]
 
   def _generate_examples(self):
     """Yields examples."""
     # TODO(VLSP): Yields (key, example) tuples from the dataset
-    yield 'key', {}
+   with tf.io.gfile.GFile(path) as f:
+      for line in f:
+        # Possible keys are:
+        # "article_id": str
+        # _DOCUMENT: list[str] article (list of paragraphs).
+        # _SUMMARY: list[str], abstract (list of paragraphs).
+        # "meta_data": list[str], list of section names.
+        # "sections": list[list[str]], list of sections (list of paragraphs)
+        d = json.loads(line)
+        summary = "\n".join(d[_SUMMARY])
+        # In original paper, <S> and </S> are not used in vocab during training or during decoding.
+        # https://github.com/armancohan/long-summarization/blob/master/data.py#L27
+        summary = summary.replace("<S>", "").replace("</S>", "")
+        yield d["article_id"], {
+            _DOCUMENT: "\n".join(d[_DOCUMENT]),
+            _SUMMARY: summary,
+            "meta_data": "\n".join(d["meta_data"])
+        }
+
