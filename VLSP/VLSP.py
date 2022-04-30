ab09ee1e26ded19acbd86b15c8d3a8f59d312dae
abhinavbohra@iitkgp.ac.in <abhinavbohra@iitkgp.ac.in>
2022-04-30 17:26:15 +0530

    Added VLSP dataset

--- VLSP/VLSP.py
+++ VLSP/VLSP.py
@@ -1,4 +1,4 @@
-"""VLSP dataset."""
+"""vlsp dataset."""
 import json
 import os
 
@@ -35,11 +35,12 @@ class VlspConfig(tfds.core.BuilderConfig):
         supported_versions=[tfds.core.Version("1.1.0")],
         **kwargs)  # pytype: disable=wrong-arg-types  # gen-stub-imports
     self.filename = filename
+    self.dl_manager = None
 
 
 
 class Vlsp(tfds.core.GeneratorBasedBuilder):
-  """DatasetBuilder for VLSP dataset."""
+  """DatasetBuilder for vlsp dataset."""
 
   BUILDER_CONFIGS = [
       VlspConfig(name="vlsp", description="Scientific Papers.")
@@ -52,7 +53,7 @@ class Vlsp(tfds.core.GeneratorBasedBuilder):
 
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
-    
+
     return tfds.core.DatasetInfo(
         builder=self,
         description=_DESCRIPTION,
@@ -66,13 +67,13 @@ class Vlsp(tfds.core.GeneratorBasedBuilder):
         citation=None,
     )
 
+
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
-    # TODO(VLSP): Downloads the data and defines the splits
-    # dl_manager is a tfds.download.DownloadManager that can be used to
-    # download and extract URLs
+    # TODO(vlsp): Downloads the data and defines the splits
+    # dl_manager is a tfds.download.DownloadManager that can be used to download and extract URLs
     # dl_paths = dl_manager.download_and_extract(_URLS)
-    path = os.path.join("VLSP")
+    path = os.path.join("vlsp")
     print(f"Path: {path}")
 
     return [
@@ -90,22 +91,25 @@ class Vlsp(tfds.core.GeneratorBasedBuilder):
         ),
     ]
 
+
   def _generate_examples(self, path=None):
     """Yields examples."""
-    # TODO(VLSP): Yields (key, example) tuples from the dataset
+    # TODO(vlsp): Yields (key, example) tuples from the dataset
     with tf.io.gfile.GFile(path) as f:
       for line in f:
         # Possible keys are:
         # "article_id": str
-        # _DOCUMENT: list[str] article (list of paragraphs).
-        # _SUMMARY: list[str], abstract (list of paragraphs).
+        # "_DOCUMENT": list[str] article (list of paragraphs).
+        # "_SUMMARY": list[str], abstract (list of paragraphs).
         # "meta_data": list[str], list of section names.
-        # "sections": list[list[str]], list of sections (list of paragraphs)
-        d = json.loads(line)
-        summary = "\n".join(d[_SUMMARY])
+        
         # In original paper, <S> and </S> are not used in vocab during training or during decoding.
         # https://github.com/armancohan/long-summarization/blob/master/data.py#L27
+        
+        d = json.loads(line)
+        summary = "\n".join(d[_SUMMARY])
         summary = summary.replace("<S>", "").replace("</S>", "")
+
         yield d["article_id"], {
             _DOCUMENT: "\n".join(d[_DOCUMENT]),
             _SUMMARY: summary,
