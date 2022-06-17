d6a3cbf630cfe362790d3209dfef4e62b4aa2401
abhinav-bohra <abhinavbohra@iitkgp.ac.in>
2022-06-17 22:03:00 +0000

    Changes for MatchSum

--- MatchSum/train_matching.py
+++ MatchSum/train_matching.py
@@ -59,8 +59,8 @@ def train_model(args):
     model = MatchSum(args.candidate_num, args.encoder)
     optimizer = Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0)
     
-    callbacks = [MyCallback(args)]
-                #  SaveModelCallback(save_dir=args.save_path, top=1)]
+    callbacks = [MyCallback(args),
+                 SaveModelCallback(save_dir=args.save_path, top=1)]
     
     criterion = MarginRankingLoss(args.margin)
     val_metric = [ValidMetric(save_path=args.save_path, data=read_jsonl(data_paths['val']))]
@@ -134,11 +134,11 @@ if __name__ == '__main__':
                         help='max learning rate for warm up', type=float)
     parser.add_argument('--margin', default=0.01,
                         help='parameter for MarginRankingLoss', type=float)
-    parser.add_argument('--warmup_steps', default=106,
+    parser.add_argument('--warmup_steps', default=1681,
                         help='warm up steps for training', type=int)
     parser.add_argument('--n_epochs', default=2,
                         help='total number of training epochs', type=int)
-    parser.add_argument('--valid_steps', default=106,
+    parser.add_argument('--valid_steps', default=1681,
                         help='number of update steps for validation and saving checkpoint', type=int)
 
     args = parser.parse_known_args()[0]
