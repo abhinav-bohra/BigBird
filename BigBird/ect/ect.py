1744b8e85c9ec58281e1ce76b530f28ee5f96c00
abhinav-bohra <abhinavbohra@iitkgp.ac.in>
2022-06-18 19:36:42 +0000

    Changes for BigBird

---
+++ BigBird/ect/ect.py
@@ -0,0 +1,62 @@
+import json
+import os
+import tensorflow as tf
+import tensorflow_datasets.public_api as tfds
+
+_DOCUMENT = "article"
+_SUMMARY = "abstract"
+
+class EctConfig(tfds.core.BuilderConfig):
+  """BuilderConfig for Ect Dataset`."""
+
+  def __init__(self, *, filename=None, **kwargs):
+    super(EctConfig, self).__init__(
+        version=tfds.core.Version("1.1.1"),
+        supported_versions=[tfds.core.Version("1.1.0")],
+        **kwargs)  # pytype: disable=wrong-arg-types  # gen-stub-imports
+    self.filename = filename
+
+
+class Ect(tfds.core.GeneratorBasedBuilder):
+  """Ect Dataset`."""
+
+  def _info(self):
+    return tfds.core.DatasetInfo(
+        builder=self,
+        features=tfds.features.FeaturesDict({
+            _DOCUMENT: tfds.features.Text(),
+            _SUMMARY: tfds.features.Text(),
+        }),
+        supervised_keys=(_DOCUMENT, _SUMMARY)
+    )
+
+  def _split_generators(self, dl_manager):
+    """Returns SplitGenerators."""
+    data_dir = "/content/BigBird/BigBird/ect"
+    return [
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TRAIN,
+            gen_kwargs={"path": os.path.join(data_dir, "train.txt")},
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.VALIDATION,
+            gen_kwargs={"path": os.path.join(data_dir, "val.txt")},
+        ),
+        tfds.core.SplitGenerator(
+            name=tfds.Split.TEST,
+            gen_kwargs={"path": os.path.join(data_dir, "test.txt")},
+        ),
+    ]
+
+  def _generate_examples(self, path=None):
+    """Yields examples."""
+    with tf.io.gfile.GFile(path) as f:
+      for line in f:
+        d = json.loads(line)
+        article = "\n".join(d["article_text"])
+        summary = "\n".join(d["abstract_text"])
+        summary = summary.replace("<S>", "").replace("</S>", "")
+        yield d["article_id"], {
+            _DOCUMENT: article,
+            _SUMMARY: summary
+        }
---
+++ BigBird/ect/ect_preprocess.py
@@ -0,0 +1,36 @@
+import os, json
+
+def preprocessSummaries(s_):
+    s = []
+    for line in s_:
+        if line.startswith("Speaker ::") or line=="\n":
+            continue
+        else:
+            line = line.replace('\n',"").replace("  "," ")
+            line = "<S> " + str(line) + " </S>"
+            s.append(line)
+    return s
+
+splits = ["train", "test", "val"] 
+data = []
+
+for split in splits:
+  path_articles = f"/content/BigBird/data/final/exp1/{split}/ects" 
+  path_summaries = f"/content/BigBird/data/final/exp1/{split}/gt_summaries"
+  outfile = f"/content/BigBird/BigBird/ect/{split}.txt"
+  articles = os.listdir(path_articles)
+  summaries = os.listdir(path_summaries)
+  print(split, len(articles), len(summaries))
+  for article in articles:
+    data_point = {}
+    a = open(os.path.join(path_articles, article), 'r').readlines()
+    s = preprocessSummaries( open(os.path.join(path_summaries, article), 'r').readlines() )
+    data_point["article_id"] = article
+    data_point["article_text"] = a
+    data_point["abstract_text"] = s
+    data_str = json.dumps(data_point)
+    data.append(data_str)
+  with open(outfile, mode='wt', encoding='utf-8') as myfile:
+    myfile.write('\n'.join(data))
+    print(f"{split} done")
+  myfile.close()
