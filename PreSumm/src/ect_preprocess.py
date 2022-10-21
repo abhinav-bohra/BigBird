4ecdd02f8dbc9cc35a6d7e2f1b318977f3c6b2c1
abhinav-bohra <abhinavbohra@iitkgp.ac.in>
2022-10-21 13:07:58 +0000

    Changes for BertSum

--- PreSumm/src/ect_preprocess.py
+++ PreSumm/src/ect_preprocess.py
@@ -37,53 +37,55 @@ def preproces(s_, nlp):
       s.append(tokens)
     return s
 
-def create_dataset(splits):  
+
+def create_dataset(splits, exps):  
     nlp = stanza.Pipeline(lang='en', processors='tokenize,lowercase')  
-    for split in splits:
-        base_path = "/content/Long-Text-Summarization/data/final/exp1"
-        path_articles = f"{base_path}/{split}/ects"
-        path_summaries = f"{base_path}/{split}/gt_summaries"
-        
-        if split=="val":
-            split="valid"
-
-        outfile = f"/content/Long-Text-Summarization/PreSumm/json_data/ect.{split}.json"
-        articles = os.listdir(path_articles)
-        summaries = os.listdir(path_summaries)
-        assert (len(articles)==len(summaries))
-
-        data = []
-        for article in tqdm(articles):
-            data_point = {}
-            a_ = open(os.path.join(path_articles, article), 'r').readlines()
-            a = preproces(a_, nlp)
-            s_ = open(os.path.join(path_summaries, article), 'r').readlines()
-            s = preproces(s_, nlp)
+    for exp in exps:
+        for split in splits:
+            base_path = f"/content/Long-Text-Summarization/data/final/{exp}"
+            path_articles = f"{base_path}/{split}/ects"
+            path_summaries = f"{base_path}/{split}/gt_summaries"
             
-            #skip empty files
-            if len(a_)==0 or len(s_)==0:
-              os.remove(os.path.join(path_articles, article))
-              os.remove(os.path.join(path_summaries, article))
-              print(f"Deleting {article} as it is empty")
+            if split=="val":
+                split="valid"
 
-            data_point["src"] = a
-            data_point["tgt"] = s
-            data_point["article_id"] = article
-            data.append(data_point)
+            outfile = f"/content/Long-Text-Summarization/PreSumm/json_data/{exp}/ect.{split}.json"
+            articles = os.listdir(path_articles)
+            summaries = os.listdir(path_summaries)
+            assert (len(articles)==len(summaries))
 
-        with open(outfile, "w") as myfile:
-            json.dump(data, myfile)
-            print(f"{split} has {len(data)} instances.")
-        myfile.close()
-        
+            data = []
+            for article in tqdm(articles):
+                data_point = {}
+                a_ = open(os.path.join(path_articles, article), 'r').readlines()
+                a = preproces(a_, nlp)
+                s_ = open(os.path.join(path_summaries, article), 'r').readlines()
+                s = preproces(s_, nlp)
+                
+                #skip empty files
+                if len(a_)==0 or len(s_)==0:
+                    os.remove(os.path.join(path_articles, article))
+                    os.remove(os.path.join(path_summaries, article))
+                    print(f"Deleting {article} as it is empty")
 
-def merge_JsonFiles(filename):
+                data_point["src"] = a
+                data_point["tgt"] = s
+                data_point["article_id"] = article
+                data.append(data_point)
+
+            with open(outfile, "w") as myfile:
+                json.dump(data, myfile)
+                print(f"{split} has {len(data)} instances.")
+            myfile.close()
+            
+
+def merge_JsonFiles(filename, exp):
     result = list()
     for f1 in filename:
         with open(f1, 'r') as infile:
             result.extend(json.load(infile))
 
-    with open("/content/Long-Text-Summarization/PreSumm/json_data/ect.all.json", 'w') as output_file:
+    with open(f"/content/Long-Text-Summarization/PreSumm/json_data/{exp}/ect.all.json", 'w') as output_file:
         json.dump(result, output_file)
         print(f"Combined Set has {len(result)} instances.")
     output_file.close()
@@ -91,9 +93,11 @@ def merge_JsonFiles(filename):
 
 if __name__ == "__main__":
     splits = ["train", "test", "val"] 
-    files= ["/content/Long-Text-Summarization/PreSumm/json_data/ect.train.json",
-            "/content/Long-Text-Summarization/PreSumm/json_data/ect.valid.json",
-            "/content/Long-Text-Summarization/PreSumm/json_data/ect.test.json"]
+    exps = ["exp1", "exp2"]
+    for exp in exps:
+        files= [f"/content/Long-Text-Summarization/PreSumm/json_data/{exp}/ect.train.json",
+                f"/content/Long-Text-Summarization/PreSumm/json_data/{exp}/ect.valid.json",
+                f"/content/Long-Text-Summarization/PreSumm/json_data/{exp}/ect.test.json"]
 
-    create_dataset(splits)
-    merge_JsonFiles(files)
+        create_dataset(splits, exps)
+        merge_JsonFiles(files, exp)
